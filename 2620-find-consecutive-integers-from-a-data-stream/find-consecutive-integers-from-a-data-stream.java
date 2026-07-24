37ae587442ab4d30074a73e8ff19173a43dd63e5
rishi2505-RK <rishirishi06260@gmail.com>
2026-07-24 12:17:33 +0530

    Time: 26 ms (86.03%) | Memory: 104.2 MB (70.39%) - LeetSync

---
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.java
@@ -0,0 +1,20 @@
+class DataStream {
+    private final int value;
+    private final int k;
+    private int count;
+
+    public DataStream(int value, int k) {
+        this.value = value;
+        this.k = k;
+        this.count = 0;
+    }
+
+    public boolean consec(int num) {
+        if (num == value) {
+            count++;
+        } else {
+            count = 0;
+        }
+        return count >= k;
+    }
+}
